2baa9708c39ed67666faece03add5867a2318588
Elias Bosire Ayunga <bosire.elias@gmail.com>
2024-12-18 22:05:01 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the paragraph element with the id "text"
+    const paragraph = document.getElementById("text");
+
+    // Change its text content
+    paragraph.textContent = "This is really cool!";
+});
